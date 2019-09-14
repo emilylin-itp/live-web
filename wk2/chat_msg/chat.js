be6943e9189f6ae60374d97b52c56c26026ced2a
emilylin-itp.github.io <el3015@nyu.edu>
2019-09-14 10:54:10 -0400

    got rid of not working randomize message fnc

--- wk2/chat_msg/chat.js
+++ wk2/chat_msg/chat.js
@@ -1,7 +1,7 @@
 function init() {
     let randomizeButton = document.getElementById('bttn');
 
-    //when button click perform getOriginal function
+    //when button is clicked perform getOriginal function
     randomizeButton.addEventListener('click', getOriginal);
 }
 
@@ -18,19 +18,28 @@ function getOriginal() {
     //console og message + var type
     console.log(ogMessage, typeof ogMessage);
 
-    //perform replace function
+    //call randomizeMessage function
     randomizeMessage(ogMessage);
 }
 
-function randomizeMessage(ogMessage) {
-    let array = ogMessage.split('');
-    let i = Math.floor(Math.random() * array.length);
+function randomizeMessage(x) {
+    //make the string into an array
+    let array = x.split('');
 
-    array[i] = String.fromCharCode(array[i].charCodeAt(0) + 2 * Math.floor(Math.random() * 2) - 1);
-    return array.join('');
+    //random number of characters to replace
+    let randomNum = Math.floor(Math.random() * array.length); 
 
+    //everytime you go through for loop 
+    for(let i = 0; i < )
 }
 
 // console.log(replace('123456abcdef'));
 
 window.addEventListener('load', init);	
+
+
+    // //onlny replaces one character
+    // gets unicode character of --> going and finding utf of character --> adding random number changes index of utf, string fromCharCode 
+    // there is a library utf, to access letters and numbers you need to say "String.fromCharCode", then you give it a number,
+    // array[i] = String.fromCharCode(array[i].charCodeAt(0) + 2 * Math.floor(Math.random() * 2) - 1);
+    // return array.join('');
