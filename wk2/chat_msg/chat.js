65a1f285e8aabb2e7fafde1e0e2521213717e3f1
emilylin-itp.github.io <el3015@nyu.edu>
2019-09-14 11:25:52 -0400

    Update chat.js

--- wk2/chat_msg/chat.js
+++ wk2/chat_msg/chat.js
@@ -1,3 +1,6 @@
+//array for replacing characters with new ones
+let globalChar = ["a", "b", "c", "d", "e", "f", "g"];
+
 function init() {
     let randomizeButton = document.getElementById('bttn');
 
@@ -28,20 +31,31 @@ function randomizeMessage(x) {
 
     //random number of characters to replace
     let randomNum = Math.floor(Math.random() * array.length); 
+    console.log("random num is " + randomNum);
 
     //getting a random index from for loop
-    for(let i = 0; i < array.length;i++){
+    for(let i = 0; i < randomNum; i++){
         let randomIndex = Math.floor(Math.random() * array.length); 
+        console.log(randomIndex);
+
+        let randomChar = Math.floor(Math.random()* globalChar.length);
+        console.log(randomChar);
+
         //find a character at that specific random number and then replace it with ?
-        array[randomIndex] = "?"; 
+        array[randomIndex] = "?";
+        //make into a new string
     }
+    return array.join('');
 }
 
-console.log(randomizeMessage('123456abcdef'));
+console.log(randomizeMessage('hello this is my message'));
 
 window.addEventListener('load', init);	
 
 
+
+
+////////////////////////DUMPSTER CODE//////////////////////////
     // //onlny replaces one character
     // gets unicode character of --> going and finding utf of character --> adding random number changes index of utf, string fromCharCode 
     // there is a library utf, to access letters and numbers you need to say "String.fromCharCode", then you give it a number,
