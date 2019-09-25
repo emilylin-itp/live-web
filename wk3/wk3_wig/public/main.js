be7412fc3658bcc7a6a89bb56aa0d7c86ca4463d
Emily Lin <el3015@nyu.edu>
2019-09-25 08:59:31 -0400

    redo button working

--- wk3/wk3_wig/public/main.js
+++ wk3/wk3_wig/public/main.js
@@ -6,63 +6,88 @@
 //3. on click the client should send a "drawLine" message to the server when we are moving the mouse.
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    var mouse = { 
-       click: false,
-       move: false,
-       pos: {x:0, y:0},
-       pos_prev: false
+document.addEventListener("DOMContentLoaded", function () {
+    var mouse = {
+        click: false,
+        move: false,
+        pos: { x: 0, y: 0 },
+        pos_prev: false
     };
     // get canvas element and create context
-    var canvas  = document.getElementById('drawing');
+    var canvas = document.getElementById('drawing');
     var context = canvas.getContext('2d');
-    var width   = window.innerWidth;
-    var height  = window.innerHeight;
-    var socket  = io.connect();
- 
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var socket = io.connect();
+
     // set canvas to full browser width/height
     canvas.width = width;
     canvas.height = height;
- 
+
     // register mouse event handlers
-    canvas.onmousedown = function(e){ mouse.click = true; };
-    canvas.onmouseup = function(e){ mouse.click = false; };
- 
-    canvas.onmousemove = function(e) {
-       // normalize mouse position to range 0.0 - 1.0
-       mouse.pos.x = e.clientX / width;
-       mouse.pos.y = e.clientY / height;
-       mouse.move = true;
+    canvas.onmousedown = function (e) { mouse.click = true; };
+    canvas.onmouseup = function (e) { mouse.click = false; };
+
+    canvas.onmousemove = function (e) {
+        // normalize mouse position to range 0.0 - 1.0
+        mouse.pos.x = e.clientX / width;
+        mouse.pos.y = e.clientY / height;
+        mouse.move = true;
     };
- 
+
     // draw line received from server
-     socket.on('drawLine', function (data) {
-       var line = data.line;
-       context.beginPath();
-       context.moveTo(line[0].x * width, line[0].y * height);
-       context.lineTo(line[1].x * width, line[1].y * height);
-       context.lineWidth = 7;
-       context.lineCap = 'round';
-       context.stroke();
+    socket.on('drawLine', function (data) {
+        var line = data.line;
+        context.beginPath();
+        context.moveTo(line[0].x * width, line[0].y * height);
+        context.lineTo(line[1].x * width, line[1].y * height);
+        context.lineWidth = 7;
+        context.lineCap = 'round';
+        context.stroke();
     });
-    
+
+    //function to clear canvas
+    function emitAndCanvas() {
+        socket.emit('clear');
+        clearCanvas();
+        console.log('emitAndCanvas works!');
+    }
+
+    function clearCanvas() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        console.log('clearCanvas works!');
+    }
+
+
+    //redo button clicked -> clear canvas
+    let redoButton = document.getElementById('redo');
+    redoButton.addEventListener('click', () => {
+        console.log('redo button clicked!');
+        emitAndCanvas();
+    });
+
+
+    // this will handle the socket event and clears the canvas
+    socket.on('clear', clearCanvas);
+
+
+
     // main loop, running every 25ms
     function mainLoop() {
-       // check if the user is drawing
-       if (mouse.click && mouse.move && mouse.pos_prev) {
-          // send line to to the server
-          socket.emit('drawLine', { 
-              line: [ mouse.pos, mouse.pos_prev ] 
+        // check if the user is drawing
+        if (mouse.click && mouse.move && mouse.pos_prev) {
+            // send line to to the server
+            socket.emit('drawLine', {
+                line: [mouse.pos, mouse.pos_prev]
             });
-          mouse.move = false;
-       }
-       mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
-       setTimeout(mainLoop, 2);
+            mouse.move = false;
+        }
+        mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
+        setTimeout(mainLoop, 25);
     }
     mainLoop();
- });
 
-
+});
 
 
 /*
@@ -121,8 +146,8 @@ function init() {
         // check if the user is drawing
         if (mouse.click && mouse.move && mouse.pos_prev) {
             // send line to to the server
-            socket.emit('draw_line', { 
-                line: [mouse.pos, mouse.pos_prev] 
+            socket.emit('draw_line', {
+                line: [mouse.pos, mouse.pos_prev]
             });
             mouse.move = false;
         }
