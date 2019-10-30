7d49fadafad4eb431d6d572339c98a902b6ac47c
Emily Lin <el3015@nyu.edu>
2019-10-30 03:25:35 -0400

    record working. img added to window

--- wk8/wk8_hw_main/script.js
+++ wk8/wk8_hw_main/script.js
@@ -11,6 +11,7 @@ let mouseY = 0;
 let rangeValueRed;
 let rangeValueBlue;
 let rangeValueThreshold;
+let evl;
 
 // this array will contain "chunks" of the video captured by the MediaRecorder
 let chunks = [];
@@ -59,7 +60,19 @@ function init() {
 
     // on click -- start recording 
     recButton = document.getElementById('rec-h2');
-    recButton.addEventListener('click', startRecord);
+
+
+    evl = false;
+    recButton.addEventListener('click', () => {
+
+        if (evl !== false) {
+            mediaRecorder.stop();
+            console.log('stop record!');
+        } else {
+            startRecord();
+            evl = true;
+        }
+    });
 }
 
 
@@ -74,12 +87,10 @@ function setupCanvas() {
 }
 
 
-
 function drawCanvas() {
     //draw image onto canvas
     //syntax: void ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
-    context.drawImage(video, 0, 0, video.width, video.height, -300, 0, canvas.width * 2.5, canvas.height * 2.5);
-
+    context.drawImage(video, 0, 0, video.width, video.height, -300, 0, canvas.width * 2, canvas.height * 2);
 
     // //get values for each slider
     rangeValueRed = document.getElementById('sliderRangeRed').value;
@@ -119,7 +130,7 @@ function drawCanvas() {
         /* if brightness, then make green pixels be more prominent */
         if (bright > threshold) {
             // imageData.data[i] = rangeValueThreshold*5;
-            imageData.data[i + 1] = rangeValueThreshold * 5;
+            imageData.data[i + 1] = rangeValueThreshold + 200;
             // imageData.data[i + 2] = rangeValueThreshold*5;
             // imageData.data[i + 3] = rangeValueThreshold*5;
             // imageData.data[i + 2] = 0;
@@ -132,28 +143,26 @@ function drawCanvas() {
     requestAnimationFrame(drawCanvas);
 }
 
+//function:
+// 1. record button hit
+// 2. start record + change text to "stop"
+// 3. if button hit while "start record" --> stop record + change text back to "rec
 
 function startRecord() {
-    recButton = document.getElementById('rec-h2');
-    recButton.fill = '#FF0000';
+    recText = document.getElementById('rec-h2');
+    recText.style.fill = '#FF0000';
+    recText.innerHTML = 'STOP';
+
+    // circleButton = document.getElementById('circle-svg');
+    // svg.style.fill = "#FF0000";
+
+    // circleButton.style.fill = "#FF000";
     console.log('start recording!');
 
-    let theData = canvas.toDataURL('image/jpeg', 1);
-    console.log(theData);
-    socket.emit('imagedata', theData);
 
-    console.log("starting recording!");
-
-    // Start the MediaRecorder
-    mediaRecorder.start();
-
-    // After 2 seconds, stop the MediaRecorder
-    setTimeout(function () {
-        mediaRecorder.stop();
-    }, 10000);
-
-    console.log("recording stop!");
+    mediaRecorder.start(); // Start the MediaRecorder
 }
+
 
 
 function setupMediaRecorder() {
@@ -163,6 +172,12 @@ function setupMediaRecorder() {
     mediaRecorder.onstop = function (e) {
         console.log("stop");
 
+        //change inner text 
+        recText.innerHTML = 'RECORD';
+        recText.style.fill = '#FFF';
+
+        // svg.style.fill = "#FFF";
+
         // Create a new video element on the page
         var video = document.createElement('video');
         video.controls = true;
@@ -171,11 +186,23 @@ function setupMediaRecorder() {
         var blob = new Blob(chunks, { 'type': 'video/webm' });
         // Generate a URL for the blob
         var videoURL = window.URL.createObjectURL(blob);
+
         // Make the video element source point to that URL
         video.src = videoURL;
 
         // Put the video element on the page
         // document.body.appendChild(video);
+
+        let theData = canvas.toDataURL('image/jpeg', 1);
+        console.log(theData);
+
+
+        let myWindow = window.open("", "MsgWindow", "width=500,height=500");//new window
+        myWindow.document.write('<img src="'+theData+'"/>'); //writes the data to the window
+
+        //window.location.href = theData; //save locally doesn't work
+
+        socket.emit('imagedata', theData); //emit image data
 
         //blob is binary data
         socket.emit('video', blob);
@@ -211,3 +238,38 @@ function setupMediaRecorder() {
 
 //     console.log("recording stop!");
 // });
+
+// function stopRecord() {
+//     mediaRecorder.stop();
+//     // console.log("recorder stopped, data available");
+
+//     // //style
+//     // recText.innerHTML = 'REC';
+//     // recText.style.fill = '#FFF';
+//     // // circleButton.style.fill = "#C4C4C5";
+
+//     // let theData = canvas.toDataURL('image/jpeg', 1);
+//     // console.log(theData);
+//     // socket.emit('imagedata', theData);
+// }
+
+    // let theData = canvas.toDataURL('image/jpeg', 1);
+    // console.log(theData);
+    // socket.emit('imagedata', theData);
+
+    // stop.onclick = function () {
+    //     //stop
+    //     mediaRecorder.stop();
+    //     console.log("recorder stopped, data available");
+
+    //     //style
+    //     recText.innerHTML = 'REC';
+    //     recText.style.fill = '#FFF';
+    //     // circleButton.style.fill = "#C4C4C5";
+    // }
+
+
+    // // // After 2 seconds, stop the MediaRecorder
+    // // setTimeout(function () {
+    // //     mediaRecorder.stop();
+    // // }, 10000);
